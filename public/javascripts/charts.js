65611dccdb5b0f0c770b772db502ccb5d6281625
breadethewbaker <bmbaker@buffalo.edu>
2018-04-10 22:35:19 -0400

    MADE A CHART

---
+++ public/javascripts/charts.js
@@ -0,0 +1,37 @@
+//var Chart = require('chart.js');
+let ctx = document.getElementById("bldgMoneyChart").getContext('2d');
+let bldgMoneyChart = new Chart(ctx, {
+type: 'bar',
+data: {
+  labels: ["Alumni", "Baird", "Baldy", "Bell", "Bonner"],
+  datasets: [{
+    label: 'Annual Energy Cost',
+    data: [10, 11, 12, 13, 14],
+    //data: [710250, 175047, 332302, 221077, 135152],
+    backgroundColor: [
+      'rgba(255, 99, 132, 0.2)',
+      'rgba(54, 162, 235, 0.2)',
+      'rgba(255, 206, 86, 0.2)',
+      'rgba(75, 192, 192, 0.2)',
+      'rgba(153, 102, 255, 0.2)'
+    ],
+    borderColor: [
+      'rgba(255,99,132,1)',
+      'rgba(54, 162, 235, 1)',
+      'rgba(255, 206, 86, 1)',
+      'rgba(75, 192, 192, 1)',
+      'rgba(153, 102, 255, 1)'
+    ],
+    borderWidth: 1
+  }]
+},
+options: {
+  scales: {
+    yAxes: [{
+      ticks: {
+        beginAtZero:true
+      }
+    }]
+  }
+}
+});
