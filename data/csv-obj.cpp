e3c9f48267dd434b37876ef12d22f6554fbcc2bb
breadethewbaker <bmbaker@buffalo.edu>
2018-04-17 17:20:10 -0400

    building the csv reader

--- data/csv-obj.cpp
+++ data/csv-obj.cpp
@@ -14,14 +14,24 @@ struct bldg {
 int main(int argc, char* argv[]) {
 	ifstream file ("data.csv");
 	ostream oFile ("out.json");
+	string line;
+	bool open;
+	bool bldg;
+	int ln = 0;
+	int rln = 0;
 	if ( file.is_open() )
 	{
-		string line;
-		bool open;
 		while ( getline (file,line) )
 		{
-			if ( line.find("hall") != npos || line.find("bldg") != npos)
+			if ( line.find("SQFEET") != npos )
 			{
+				int cc = 0;
+				string bldg;
+				for (string::iterator i=line.begin(); i!=line.end(); ++i)
+				{
+					if ( cc == 2 ) bldg.append((string)(*i));
+					if ( *i == ',' ) ++cc;
+				}
 				
 			}
 		}
