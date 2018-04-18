f074419d1d823a399bb8e2533b709ef7a2b8c169
breadethewbaker <bmbaker@buffalo.edu>
2018-04-18 16:54:43 -0400

    still working on the data reader script

--- data/csv-obj.cpp
+++ data/csv-obj.cpp
@@ -15,24 +15,48 @@ int main(int argc, char* argv[]) {
 	ifstream file ("data.csv");
 	ostream oFile ("out.json");
 	string line;
-	bool open;
-	bool bldg;
-	int ln = 0;
-	int rln = 0;
+	
+	bool kwh = false;
+	bool natGas = false;
+	int counter = 0;
+	
 	if ( file.is_open() )
 	{
+		file << "{\n\t\"buildings\": [\n\t\t";
 		while ( getline (file,line) )
 		{
 			if ( line.find("SQFEET") != npos )
 			{
 				int cc = 0;
-				string bldg;
+				string bldg = "{\n\t\t\t\"name\": "
 				for (string::iterator i=line.begin(); i!=line.end(); ++i)
 				{
 					if ( cc == 2 ) bldg.append((string)(*i));
 					if ( *i == ',' ) ++cc;
 				}
+				bldg.append(",\n\t\t\t");
+				file << bldg;
 				
+			}
+			if ( kwh && counter>0 && counter%2 == 0 )
+			{
+				
+			}
+			if ( natGas && counter>0 && counter%2 == 0 )
+			{
+				
+			}
+			if ( line.find("KWH") != npos )
+			{
+				file << "\"KWH\": [\n\t\t\t\t";
+				kwh = true;
+				++counter;
+			}
+			if ( line.find("CCF") != npos )
+			{
+				file << "\"CCF\": [\n\t\t\t\t";
+				natGas = true;
+				++counter;
 			}
 		}
 	}
