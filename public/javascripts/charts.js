6c7166dfee5217e9321d581006fdc8d319ddacce
pururval <pururval@buffalo.edu>
2018-04-23 22:12:46 -0400

    few updates on layout

--- public/javascripts/charts.js
+++ public/javascripts/charts.js
@@ -3,12 +3,17 @@ let ctx = document.getElementById("bldgMoneyChart").getContext('2d');
 let bldgMoneyChart = new Chart(ctx, {
 type: 'bar',
 data: {
-  labels: ["Alumni", "Baird", "Cold Water Plant", "Baldy", "John Beane Center", "Bell", "Bissell", "Bonner"],
+  labels: ["Alumni", "Baird", "Cold Water Plant", "Baldy", "John Beane Center", "Bell", "Bissell", "Bonner", "Center for the Arts", "Center for Tomorrow", "North Child Care", "Clemens Hall", "CNG Pumps", "Computing Center"],
   datasets: [{
     label: 'Annual Energy Cost',
-    data: [710249.82, 175046.66, 698843.38, 332301.95, 104673.20, 221076.73, 16246.27, 135151.52],
+    data: [710249.82, 175046.66, 698843.38, 332301.95, 104673.20, 221076.73, 16246.27, 135151.52, 448134.77, 38612.73, 18010.19, 171590.57, 12306.30, 51814.44  ],
     //data: [710250, 175047, 332302, 221077, 135152],
     backgroundColor: [
+      'rgba(255, 99, 132, 0.2)',
+      'rgba(54, 162, 235, 0.2)',
+      'rgba(255, 206, 86, 0.2)',
+      'rgba(75, 192, 192, 0.2)',
+      'rgba(153, 102, 255, 0.2)',
       'rgba(255, 99, 132, 0.2)',
       'rgba(54, 162, 235, 0.2)',
       'rgba(255, 206, 86, 0.2)',
@@ -31,12 +36,17 @@ data: {
       'rgba(255, 206, 86, 1)',
       'rgba(75, 192, 192, 1)',
       'rgba(153, 102, 255, 1)',
+      'rgba(255, 99, 132, 0.2)',
+      'rgba(54, 162, 235, 0.2)',
+      'rgba(255, 206, 86, 0.2)',
+      'rgba(75, 192, 192, 0.2)',
+      'rgba(153, 102, 255, 0.2)'
     ],
     borderWidth: 1
   }]
 },
 options: {
-  
+
   scales: {
     yAxes: [{
       ticks: {
